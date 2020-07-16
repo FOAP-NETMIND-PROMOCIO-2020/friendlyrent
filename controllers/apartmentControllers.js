bd8c3d4bdaa39b67678754b24c3840fae67f4b79
Cecilia Mota <cmotar2@gmail.com>
2020-07-16 13:26:15 +0200

    Formulario add-new-apartment. Ruta post y get creadas

--- controllers/apartmentControllers.js
+++ controllers/apartmentControllers.js
@@ -1,3 +1,12 @@
 exports.getAllApartments = (req, res) => {
     res.render('index');
 }
+
+exports.getNewApartment = (req, res) => {
+    res.render('new-apartment');
+}
+
+exports.postNewApartment = (req, res) => {
+    res.send("Hemos recibido los datos del apartamento");
+    console.log("Datos nuevo apartamento", req.body);
+}
--- routes/apartmentRoutes.js
+++ routes/apartmentRoutes.js
@@ -5,5 +5,8 @@ const router = express.Router();
 const apartmentControllers = require('../controllers/apartmentControllers');
 
 router.get('/', apartmentControllers.getAllApartments);
+router.get('/new-apartment', apartmentControllers.getNewApartment);
+
+router.post('/new-apartment', apartmentControllers.postNewApartment);
 
 module.exports = router;
